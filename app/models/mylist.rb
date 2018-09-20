6103e2bb7560e783d99af454d005ee3efd0067dd
Yuuki Sone <yuki_sone@dwango.co.jp>
2018-09-21 03:10:05 +0900

    niconico_mylist_id is uniqueness

--- app/models/mylist.rb
+++ app/models/mylist.rb
@@ -3,4 +3,6 @@
 class Mylist < ApplicationRecord
   has_many :twitter_accounts, through: :twitter_account_mylists
   has_many :twitter_account_mylists
+
+  validates :niconico_mylist_id, presence: true, uniqueness: true
 end
--- db/schema/mylist.rb
+++ db/schema/mylist.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 create_table :mylists, force: :cascade do |t|
-  t.bigint :niconico_mylist_id, null: false, index: true
+  t.bigint :niconico_mylist_id, null: false, index: { unique: true }
 
   t.datetime :created_at, null: false
   t.datetime :updated_at, null: false
