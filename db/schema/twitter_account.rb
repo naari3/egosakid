9f384ab51477b4c9a740f6623aa64dd9d38c2d11
Yuuki Sone <yuki_sone@dwango.co.jp>
2018-09-21 03:06:11 +0900

    add some restriction

--- db/schema/twitter_account.rb
+++ db/schema/twitter_account.rb
@@ -1,10 +1,10 @@
 # frozen_string_literal: true
 
 create_table :twitter_accounts, force: :cascade do |t|
-  t.bigint :twitter_user_id
-  t.string :access_token
-  t.string :access_secret
-  t.boolean :authenticated
+  t.bigint :twitter_user_id, null: false, index: true
+  t.string :access_token, null: false
+  t.string :access_secret, null: false
+  t.boolean :authenticated, null: false
 
   t.datetime :created_at, null: false
   t.datetime :updated_at, null: false
