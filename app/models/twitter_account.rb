b2fc356f81795783482a25fd4c7a63413561ea9b
Yuuki Sone <yuki_sone@dwango.co.jp>
2018-09-21 03:13:04 +0900

    twitter_user_id is uniqueness

--- app/models/twitter_account.rb
+++ app/models/twitter_account.rb
@@ -3,4 +3,6 @@
 class TwitterAccount < ApplicationRecord
   has_many :mylists, through: :twitter_account_mylists
   has_many :twitter_account_mylists
+
+  validates :twitter_user_id, presence: true, uniqueness: true
 end
--- db/schema/twitter_account.rb
+++ db/schema/twitter_account.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 create_table :twitter_accounts, force: :cascade do |t|
-  t.bigint :twitter_user_id, null: false, index: true
+  t.bigint :twitter_user_id, null: false, index: { unique: true }
   t.string :access_token, null: false
   t.string :access_secret, null: false
   t.boolean :authenticated, null: false
