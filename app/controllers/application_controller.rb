a587483d6448c2e8642fe1332d467b9ec0f3c4e5
Yuuki Sone <yuki_sone@dwango.co.jp>
2018-09-22 19:53:02 +0900

    Implement home

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,9 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  helper_method :current_user
+
+  def current_user
+    User.find_by(id: session[:user_id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  root 'home#index'
+
   get '/auth/:provider/callback', to: 'sessions#create'
   resources :mylists, only: :show
 end
