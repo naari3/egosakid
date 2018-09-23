6f8d86cc81ada4c463b8fc0ae99d1ba623180a41
Yuuki Sone <yuki_sone@dwango.co.jp>
2018-09-23 21:47:45 +0900

    search with title

--- app/controllers/mylists_controller.rb
+++ app/controllers/mylists_controller.rb
@@ -7,6 +7,8 @@ class MylistsController < ApplicationController
 
   def show
     @mylist = Mylist.find_by!(niconico_mylist_id: params[:id])
+    @items = @mylist.niconico_mylist.items
+    @items = @items.select { |item| item.title.include? search_params[:title] } if search_params[:title]
   end
 
   def new
@@ -28,5 +30,9 @@ class MylistsController < ApplicationController
 
   def create_params
     params.require(:mylist).permit(:niconico_mylist_id)
+  end
+
+  def search_params
+    params.permit(:title)
   end
 end
